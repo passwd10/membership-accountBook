24a43536564a82a90ad06c704035484ed7c125af
passwd10 <parkinseo94@naver.com>
2020-10-05 10:39:58 +0900

    서버 개발환경 구축
    express-generator로 서버 구축
    eslint로 lint
    dotenv로 환경변수관리
    nodemon으로 서버자동화

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+app.use((req, res) => {
+  res.status(404).send('error');
+});
+
+module.exports = app;
