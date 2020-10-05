654351d6938efc4c579f36f9f35d06c9e3b03193
passwd10 <parkinseo94@naver.com>
2020-10-05 19:01:04 +0900

    미들웨어에 CORS 설정

--- server/app.js
+++ server/app.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
-const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+app.use('/signIn', authRouter);
 
 app.use((req, res) => {
   res.status(404).send('error');
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
+  console.log('hi');
   res.send('hello world');
 });
 
