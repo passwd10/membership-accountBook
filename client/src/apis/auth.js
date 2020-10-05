47d4ad505c9f232dda2ca150c55433bf0c15374f
passwd10 <parkinseo94@naver.com>
2020-10-05 18:50:05 +0900

    클라이언트에 로그인 서비스 구현
    axios를 이용한 비동기 통신

---
+++ client/src/apis/auth.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const URL = 'http://localhost:3000';
+
+const signIn = async ({ userId, userPassword }) => {
+  try {
+    const data = await axios.post(`${URL}/signIn`, { userId, userPassword });
+    return data;
+  } catch (error) {
+    console.error('login fail');
+    return false;
+  }
+};
+
+export { signIn };
