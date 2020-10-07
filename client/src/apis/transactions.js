9e3fd538f70cd726ee9636704b3175110b1b410e
passwd10 <parkinseo94@naver.com>
2020-10-08 02:48:59 +0900

    거래내역 테스트 api 구현

--- client/src/apis/transactions.js
+++ client/src/apis/transactions.js
@@ -6,10 +6,19 @@ const token = document.cookie.split('=')[1];
 
 axios.defaults.headers.common['Authorization'] = token;
 
+const dummyData = {
+  type: 'expenditure',
+  date: new Date(),
+  money: '10000',
+  content: '점심식사!!',
+  paymentMethod: '현금',
+  category: '식비',
+};
+
 const addTransactions = async () => {
   try {
     const data = await axios.post(`${URL}/transactions`,
-      { },
+      dummyData,
       { withCredentials: true });
     return data;
   } catch (error) {
