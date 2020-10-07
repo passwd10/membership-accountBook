692e7ae01ed71a96cdd0e8e16984790d93e53c68
passwd10 <parkinseo94@naver.com>
2020-10-07 16:33:04 +0900

    axios통신에 credential옵션 추가

--- client/src/apis/auth.js
+++ client/src/apis/auth.js
@@ -4,7 +4,7 @@ const URL = 'http://localhost:3000';
 
 const signIn = async ({ userId, userPassword }) => {
   try {
-    const data = await axios.post(`${URL}/signIn`, { userId, userPassword });
+    const data = await axios.post(`${URL}/signIn`, { userId, userPassword }, { withCredentials: true });
     return data;
   } catch (error) {
     console.error('login fail');
