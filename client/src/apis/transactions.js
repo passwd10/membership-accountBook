e1e3898a2ddcfbce64b245bed3583f543197c388
passwd10 <parkinseo94@naver.com>
2020-10-08 00:19:56 +0900

    request header에 token삽입

--- client/src/apis/transactions.js
+++ client/src/apis/transactions.js
@@ -2,9 +2,15 @@ import axios from 'axios';
 
 const URL = 'http://localhost:3000';
 
+const token = document.cookie.split('=')[1];
+
+axios.defaults.headers.common['Authorization'] = token;
+
 const addTransactions = async () => {
   try {
-    const data = await axios.post(`${URL}/transactions`, { }, { withCredentials: true });
+    const data = await axios.post(`${URL}/transactions`,
+      { },
+      { withCredentials: true });
     return data;
   } catch (error) {
     console.error('cannot add transactions');
