92ef9045463059d5f3cfff969e1dd95f00d12cb3
passwd10 <parkinseo94@naver.com>
2020-10-12 20:09:39 +0900

    라우터 구현

---
+++ client/src/router.js
@@ -0,0 +1,28 @@
+import LoginPage from './views/pages/LoginPage';
+import TransactionHistoryPage from './views/pages/TransactionHistoryPage';
+
+export default function Router (path) {
+  const app = document.getElementById('app');
+
+  const routes = {
+    '/login': LoginPage(),
+    '/': TransactionHistoryPage(),
+  };
+
+  const render = path => {
+    const page = routes[path];
+
+    if (!page) {
+      app.innerHTML = `${path} Not Found`;
+      return;
+    }
+
+    while (app.hasChildNodes()) {
+      app.removeChild(app.firstChild);
+    }
+
+    app.appendChild(page);
+  };
+
+  render(path);
+}
