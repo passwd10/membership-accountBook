5177c172a5567b5519135a98c9f6321c0b89f823
passwd10 <parkinseo94@naver.com>
2020-10-14 22:20:25 +0900

    거래내역 날짜별 출력기능 구현

--- client/src/views/components/TransactionLists.js
+++ client/src/views/components/TransactionLists.js
@@ -2,15 +2,46 @@ import transactionsModel from '../../models/transactionsModel';
 
 import { selector } from '../../utils/querySelector';
 
-const updateTransactionListsView = (transactions) => {
-  const transactionHistoryPage = selector('.transactionHistoryPage');
-  if (transactionHistoryPage) {
-    const transactionLists = selector('.transaction_lists', transactionHistoryPage);
-    const template = transactions.reduce((acc, transaction) => {
-      return acc += `<li>${transaction.type} ${transaction.date} ${transaction.content}</li>`;
-    }, '');
+const extractDay = (date) => date.split('-')[2].slice(0, 2);
+const extractFullDate = (date) => date.split('T')[0];
+const sortByDate = (transactions) => transactions.sort((a, b) => Number(extractDay(a.date)) - Number(extractDay(b.date)));
 
-    transactionLists.innerHTML = template;
+const sortTransactions = (transactions, allTransactions) => {
+  sortByDate(transactions).forEach(transaction => {
+    const date = extractFullDate(transaction.date);
+    allTransactions.hasOwnProperty(date) ?
+      allTransactions[date].push(transaction) :
+      allTransactions[date] = [transaction];
+  });
+  return allTransactions;
+};
+
+const getTransactionListsTemplate = (allTransactions) => {
+  let template = '';
+  for (const date in allTransactions) {
+    template += `<li class='transaction_list_date'>${date}</li>`;
+    template += allTransactions[date].reduce((acc, transaction) => acc += listTemplate(transaction), '');
+  }
+  return template;
+};
+
+const listTemplate = (transaction) => {
+  return `
+    <li class='transaction_list ${transaction.id}'>
+      ${extractFullDate(transaction.date)}
+      ${transaction.category.title}
+      ${transaction.content}
+      ${transaction.payment_method.title}
+      -${transaction.money}원
+    </li>
+    `;
+};
+
+const updateTransactionListsView = (transactions) => {
+  const transactionLists = selector('.transaction_lists');
+  if (transactionLists) {
+    const allTransactions = sortTransactions(transactions, {});
+    transactionLists.innerHTML = getTransactionListsTemplate(allTransactions);
   }
 };
 
