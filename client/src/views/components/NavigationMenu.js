a2c564f1104eb5e30c75b00cc26525cc575e54b2
passwd10 <parkinseo94@naver.com>
2020-10-14 16:54:13 +0900

    네비게이션메뉴 CSS 적용

--- client/src/views/components/NavigationMenu.js
+++ client/src/views/components/NavigationMenu.js
@@ -2,6 +2,8 @@ import { selector } from '../../utils/querySelector';
 
 import Router from '../../router';
 
+import '../../styles/NavigationMenu.css';
+
 const getTemplate = () => `
   <ul class='navigation_menu_ul'>
     <li class='history_li'>내역</li>
