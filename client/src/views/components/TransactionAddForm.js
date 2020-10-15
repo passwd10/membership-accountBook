ca09d43afaae4920986ab669622f7df86cae7846
passwd10 <parkinseo94@naver.com>
2020-10-15 14:16:54 +0900

    Form 내용 지우는 기능구현

--- client/src/views/components/TransactionAddForm.js
+++ client/src/views/components/TransactionAddForm.js
@@ -65,20 +65,44 @@ const getTemplate = () => `
 
 let formType = '';
 
+const deleteFormData = () => {
+  const transactionHistoryPage = selector('.transactionHistoryPage');
+  const date = selector('#input_date', transactionHistoryPage);
+  const category = selector('#select_categories', transactionHistoryPage);
+  const paymentMethod = selector('#select_payment_methods', transactionHistoryPage);
+  const money = selector('#input_money', transactionHistoryPage);
+  const content = selector('#input_content', transactionHistoryPage);
+
+  date.value = '';
+  category.value = '생활';
+  paymentMethod.value = '현대카드';
+  money.value = '';
+  content.value = '';
+};
+
 const transactionAddEvent = async () => {
   const transactionHistoryPage = selector('.transactionHistoryPage');
   const yearMonth = transactionsModel.year + transactionsModel.month;
+  const date = selector('#input_date', transactionHistoryPage);
+  const category = selector('#select_categories', transactionHistoryPage);
+  const paymentMethod = selector('#select_payment_methods', transactionHistoryPage);
+  const money = selector('#input_money', transactionHistoryPage);
+  const content = selector('#input_content', transactionHistoryPage);
+
   const inputData = {
     type: formType,
-    date: selector('#input_date', transactionHistoryPage).value,
-    category: selector('#select_categories', transactionHistoryPage).value,
-    paymentMethod: selector('#select_payment_methods', transactionHistoryPage).value,
-    money: selector('#input_money', transactionHistoryPage).value,
-    content: selector('#input_content', transactionHistoryPage).value,
+    date: date.value,
+    category: category.value,
+    paymentMethod: paymentMethod.value,
+    money: money.value,
+    content: content.value,
   };
 
-  await transactionsModel.addTransactions(inputData);
-  await transactionsModel.updateTransactions('all', yearMonth);
+  const result = await transactionsModel.addTransactions(inputData);
+  if (result) {
+    await transactionsModel.updateTransactions('all', yearMonth);
+    deleteFormData();
+  }
 };
 
 const formTypeEvent = (event) => {
@@ -98,12 +122,18 @@ const formTypeEvent = (event) => {
   }
 };
 
+const formButtonToDeleteEvent = () => {
+  deleteFormData();
+};
+
 const addEvents = (node) => {
   const transactionAddButton = selector('.transaction_add_button', node);
   const formType = selector('.form_type', node);
+  const formButtonToDelete = selector('.delete_form_button', node);
 
   formType.addEventListener('click', formTypeEvent);
   transactionAddButton.addEventListener('click', transactionAddEvent);
+  formButtonToDelete.addEventListener('click', formButtonToDeleteEvent);
 };
 
 export default function TransactionAddForm() {
